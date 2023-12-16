0b46e12a4712b26db22d143fe518bc942406bad4
drivingcoachhub <153846441+drivingcoachhub@users.noreply.github.com>
2023-12-16 15:19:56 +0000

    Copilot forth commit

--- comments.js
+++ comments.js
@@ -1,18 +1,46 @@
 // create Web Server
-
-// For an extra challenge, make the home page (/) be a page that says <h1>Welcome!</h1>
-
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var qs = require('querystring');
+var comments = [];
 
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  if (request.url === '/hello') {
-    response.write("<h1>Hello!</h1>");
-  } else if (request.url === '/') {
-    response.write("<h1>Welcome!</h1>");
+var server = http.createServer(function(req, res){
+  var path = req.url;
+  var method = req.method;
+
+  if (path === '/'){
+    displayHome(res);
+  } else if (path === '/comments') {
+    if (method === 'POST') {
+      handlePost(req, res);
+    } else if (method === 'GET') {
+      displayComments(res);
+    }
+  } else {
+    displayError(res);
   }
-  response.end();
 });
 
-server.listen(8000);
-console.log("Server is listening on port 8000");
+function displayHome(res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<h1>Welcome!</h1>');
+  res.write('<a href="/comments">Comments</a>');
+  res.end();
+}
+
+function displayComments(res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<h1>Comments</h1>');
+  res.write('<form method="POST" action="/comments">');
+  res.write('<input type="text" name="username" placeholder="username">');
+  res.write('<input type="text" name="email" placeholder="email">');
+  res.write('<input type="text" name="comment" placeholder="comment">');
+  res.write('<input type="submit">');
+  res.write('</form>');
+  res.write('<ul>');
+  comments.forEach(function(comment){
+    res.write('<li>');
+    res.write('<strong>' + comment.username + '</strong> - ');
+    res.write('<em>' + comment.email + '</em> - ');
+    res.write('<span>' + comment.comment + '</span>');
