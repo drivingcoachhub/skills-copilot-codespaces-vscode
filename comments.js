9e54c21907b75c646585a04835bfd69c4ea9005d
drivingcoachhub <153846441+drivingcoachhub@users.noreply.github.com>
2023-12-16 15:25:38 +0000

    Copilot sixth commit

--- comments.js
+++ comments.js
@@ -1,2 +1,13 @@
-// create Web Server
-
+function comments() {
+    return {
+        restrict: 'E',
+        scope: {
+            comment: '='
+        },
+        templateUrl: 'comments.html',
+        controller: function($scope) {
+            $scope.comment = $scope.comment || {};
+            $scope.comment.date = $scope.comment.date || new Date();
+        }
+    };
+}
